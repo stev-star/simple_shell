c12d1defcaf1f2fb207273d147bdd731d624a5d1
stev_star <muruchistephen@gmail.com>
2023-10-11 02:03:17 -0700

    shell update

---
+++ my_strdup.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+/**
+ * my_strdup - string duplication
+ * @statement: string to be duplicated
+ * Return: duplicated string
+ */
+char *my_strdup(const char *statement)
+{
+	size_t len = my_strlen(statement) + 1;
+	char *new_string = (char *)malloc(len);
+	size_t a = 0;
+
+	if (new_string == Null)
+		return (Null);
+
+	for (; a < len; a++)
+	{
+		new_string[a] = statement[a];
+	}
+	return (new_string);
+}
